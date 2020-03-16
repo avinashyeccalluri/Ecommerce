450392bf15b2a41a57cd6ebea83bf51862889fc1
avinashyeccalluri <yeccaluriavinash@gmail.com>
2020-03-17 00:48:05 +0530

    chanegs

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -6,8 +6,13 @@ const productCartSchema = new mongoose.Schema({
     product:{
         type:ObjectId,
         ref:"Product"
-    }
+    },
+    name:String,
+    count:Number,
+    price:Number
 })
+
+const productCart=mongoose.model("ProductCart",productCartSchema)
 
 const orderSchema = new mongoose.Schema({
     products:[productCartSchema],
@@ -15,10 +20,14 @@ const orderSchema = new mongoose.Schema({
     amount:{
         type:Number
     },
-    address:String,
+    address:String, 
     updated:Date,
     user:{
         type:ObjectId,
         ref:"User"
     }
 },{timestamps:true})
+
+const order=mongoose.model("Order",orderSchema)
+
+module.exports = {order,productCart}
