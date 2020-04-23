150a655a1856a216bd70e646c014d5dea9e639d7
avinashyeccalluri <yeccaluriavinash@gmail.com>
2020-04-23 20:03:13 +0530

    after the update in purchase list

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 
+const Order = require('../models/order');
+
 exports.getUserById = (req, res, next, id) => {
   console.log(id);
   
@@ -31,3 +33,69 @@ exports.getAllUsers = (req,res)=>{
     res.json(req.something)
   })
 };
+
+exports.updateUser =(req, res) =>{
+  User.findByIdAndUpdate(
+    {_id: req.profile._id},
+    {$set : req.body},
+    {new :true , useFindAndModify : false},
+    (err , user)=>{
+      if(err){
+        return res.status.json({
+          error: "The update is not successful"
+        })
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
+    }
+  )
+}
+
+exports.userPurchaseList=(req,res)=>{
+        Order.find({user: req.profile._id})
+        .populate("user", "_id name")
+        .execute((err, order)=>{
+            if(err){
+              return res.status(400).json({
+                error : "orders are not stored"
+              })
+            }
+            return res.json(order)
+        })
+}
+
+exports.pushOrderInPurchaseList= (req,res,next) =>{
+  let purchases = []
+
+  req.body.order.products.forEach(product=>{
+      purchases.push({
+        _id : product._id,
+        name :product.name,
+        description : product.description,
+        category : product.category,
+        quanity : product.quantity,
+        amount : req.body.order.amount,
+        transactionId : req.body.order.transaction_id
+      });
+  });
+
+  //store this in db
+
+  User.findOneAndUpdate(
+    {_id : req.profile.id},
+    {$push : {purchases :purchases}},
+    {new :true},
+    (err, purchase)=>{
+      if(err){
+        return res.status(400).json({
+          error : "unable to save purchase list"
+        });
+      }
+      next();
+    }
+  )
+
+
+ 
+}
