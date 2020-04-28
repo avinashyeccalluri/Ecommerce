1c62c2d2a677b9f6ef56895e5554797ac9a10077
avinashyeccalluri <yeccaluriavinash@gmail.com>
2020-04-28 18:11:30 +0530

    changes

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,7 +1,13 @@
-const product = require('../models/product');
+const Product = require('../models/product');
+
+const formidable = require("formidable");
+
+const _ = require("lodash");
+
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) =>{
-        product.findById(id)
+        Product.findById(id)
             .populate("category")
             .exec((err, product)=>{
                 if(err){
@@ -13,3 +19,150 @@ exports.getProductById = (req, res, next, id) =>{
                 next();
         })
 }
+
+exports.createProduct = (req, res) =>{
+    let form = new formidable.IncomingForm();  
+
+    form.keepExtensions = true;
+
+    //keepExtensions will trim the formats of the images that displays
+     // the form variable assigned to form requires three parameter (error , form_inputs , file(audio,video,images))
+
+    form.parse(req, (err, fields, file)=>{
+        if(err){
+            return res.status(400).json({
+                error: "The data caused some errror or problem with image"
+            });
+        }
+
+        //destructure the fields
+
+        const{ name,description ,price, category, stock} = fields;
+
+        if(
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !stock
+        ){
+            return res.status(400).json({
+                error : "Please include all the fields"
+            })
+        }
+
+        //TODO : restriction on fields
+
+        let product = new Product(fields);
+
+        //handle the file size
+
+        if(file.photo){
+            if(file.photo.size > 2097152){
+                return res.status(400).json({
+                    error : "File size is too big"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type  //type refers to whatever the image format
+        }
+
+        //save to db
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({
+                    error : "saving tshirt failes"
+                });
+            }
+
+            res.json(product);
+        })
+
+    });
+
+
+}
+
+exports.getProduct = (req, res) =>{
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
+exports.updateProduct = (req, res) =>{
+    let form = new formidable.IncomingForm();  
+
+    form.keepExtensions = true;
+
+    //keepExtensions will trim the formats of the images that displays
+     // the form variable assigned to form requires three parameter (error , form_inputs , file(audio,video,images))
+
+    form.parse(req, (err, fields, file)=>{
+        if(err){
+            return res.status(400).json({
+                error: "The data caused some errror or problem with image"
+            });
+        }
+
+        //destructure the fields        
+
+        //TODO : restriction on fields        
+
+        let product = req.product;
+
+        // THE BELOW CODE IS RESPONSINBLE FOR THE UPDATION OF THE PRODUCT
+
+        product = _.extend(product, fields)  // the fields are getting loading into the product VARIABLE
+
+        //handle the file size
+
+        // handle the file size
+
+        if(file.photo){
+            if(file.photo.size > 2097152){
+                return res.status(400).json({
+                    error : "File size is too big"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type  //type refers to whatever the image format
+        }
+
+        //save to db
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({
+                    error : "UPDATION HAVE FAILED"
+                });
+            }
+
+            res.json(product);
+        })
+
+    });
+
+
+}
+
+exports.deleteProduct = (req, res)=>{
+    let product =req.product;
+    product.remove((err, product)=>{
+        if(err){
+            return res.status(400).json({
+                error: "The product was not deleted"
+            })
+        }
+        res.json({
+            message : "Deletion was sucessful",product
+        })
+    })
+}
+
+//middleware
+exports.photo = (req, res, next)=>{
+    if(req.product.photo.data){
+        res.set("Content-Type", req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next();
+}
+
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,4 +1,4 @@
-const express = require(express);
+const express = require('express');
 
 const router = express.Router();
 
@@ -8,15 +8,24 @@ const{isSignedIn,isAuthenticated,isAdmin} = require('../controllers/auth');
 
 const{getUserById} = require('../controllers/user');
 
-const{getProductById} = require('../controllers/product');
+const{getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct} = require('../controllers/product');
 
 
 router.param('userId',getUserById);
 router.param('productId',getProductById);
 
 
+
 //actual routes
 
-router.post('/product/create/:userId',isSignedIn ,isAuthenticated, isAdmin )
+router.post('/product/create/:userId',isSignedIn ,isAuthenticated, isAdmin, createProduct )
+
+router.get('/product/:productId',getProduct);
+
+router.get('/product/photo/:productId',photo);
+
+router.put('/product/:productId/:userId',isSignedIn ,isAuthenticated, isAdmin, updateProduct)
+
+router.delete('/product/:productId/:userId',isSignedIn ,isAuthenticated, isAdmin, deleteProduct)
 
 module.exports = router;
